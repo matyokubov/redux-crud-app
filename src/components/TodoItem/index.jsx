f51b0d29f7674c105929588250848f3463b9a4ec
matyokubov <x_bomba@inbox.ru>
2023-03-29 01:45:18 +0000

    update

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useReducer, useRef } from "react";
 import { useAction } from "../../hooks/action"
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "inputMode":
+            return {
+                inputMode: {
+                    readOnly: action.payload.readOnly,
+                    style: {
+                        border: "none",
+                        backgroundColor: "#fff"
+                    } ? action.payload.readOnly :
+                    {
+                        border: "1px solid red",
+                        backgroundColor: "#777"
+                    }
+                }
+            }
+        case "updateData":
+            alert(action.payload);
+            return {
+                inputMode: {
+                    readOnly: true,
+                    style: {
+                        border: "0",
+                        backgroundColor: "#fff"
+                    }
+                }
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+}
 
 const TodoItem = ({title, id}) => {
     console.log("todo item");
     const { remove } = useAction("todoActions")
+    const [ state, dispatch] = useReducer(reducer, {
+        inputMode: {
+            readOnly: true,
+            style: {
+                border: "0",
+                backgroundColor: "#fff"
+            }
+        }
+    })
+    console.log(state);
+    const todo = useRef()
     return (
         <div>
-            {title} <button onClick={() => remove(id)}>Remove</button>
+            <input type="text" name="todo" defaultValue={title} ref={todo} readOnly={state.inputMode.readOnly} style={{...state.inputMode.style}}/>
+            {
+                !state.inputMode.readOnly && <>
+                    <button onClick={() => dispatch({type: "updateData", payload: todo.current.value})}>Save</button>
+                    <button>Cancel</button>
+                </>
+            }
+            <button onClick={() => state.inputMode.readOnly && dispatch({type: "inputMode", payload: {readOnly: false}})}>Edit</button>
+            <button onClick={() => remove(id)}>Remove</button>
         </div>
     )
 }
--- src/hooks/action.js
+++ src/hooks/action.js
@@ -4,12 +4,12 @@ const reducer = require("../redux/reducer")
 export const useAction = (actionsGroupName) => {
     const actions = reducer[actionsGroupName]
     const dispatch = useDispatch()
-    console.log(Object.keys(actions));
+    // console.log(Object.keys(actions));
     const _actions = {}
     Object.keys(actions).forEach((actionName) => {
             _actions[actionName] = (payload = undefined) => dispatch(actions[actionName](payload))
         }
     )
-    console.log(_actions);
+    // console.log(_actions);
     return _actions;
 }
